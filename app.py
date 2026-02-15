0e066c7b08628b71a5369c0fb22e6f8a67411430
2312121 <23121@burnside.school.nz>
2026-02-16 10:33:29 +1300

    Finshed cars database, onto ep 3

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
 
+DATABASE = 'Database.db'
+
+
+#initialise
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+
 
 @app.route('/')
 def home():
